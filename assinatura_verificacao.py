c0e8fd5196c5bac46c30239ffb2f5b841a318144
prdm020 <124112567+prdm020@users.noreply.github.com>
2023-12-06 02:27:37 -0300

    modificação para SHA-3

--- assinatura_verificacao.py
+++ assinatura_verificacao.py
@@ -4,7 +4,7 @@ import base64
 
 # Recebe uma mensagem e assina ela com sua chave privada
 def assinatura(mensagem, chave_pri):
-    hashM = h.sha1(mensagem).digest()
+    hashM = h.sha3_224(mensagem).digest()
 
     cifra_hashM = oaep.RSA_OLAP_enc(chave_pri, hashM)
 
@@ -15,7 +15,7 @@ def assinatura(mensagem, chave_pri):
 # Recebe uma mensagem assinada, decodifica ela com a chave publica. Caso a mensagem 
 # gerada não for igual a original, retorna false. Caso o contrário, retorna true
 def verificacao(assinatura, chave_pub, mensagem_original):
-    hashM_original = h.sha1(mensagem_original).digest()
+    hashM_original = h.sha3_224(mensagem_original).digest()
 
     cifra_hashM = base64.b64decode(assinatura)
 
